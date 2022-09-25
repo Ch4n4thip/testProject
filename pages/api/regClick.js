19d657f218cb2f6689bf71452b4081631953e031
TwinCasterS <darktwin224@gmail.com>
2022-09-25 19:05:09 +0700

    testFormVic

--- pages/api/regClick.js
+++ pages/api/regClick.js
@@ -1,15 +1,21 @@
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
     var MongoClient = require('mongodb').MongoClient;
     var url = "mongodb+srv://Admin:1234@cluster0.fv0r6up.mongodb.net/test";
 
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, async function(err, db) {
       if (err) throw err;
-      var dbo = db.db("Ject_Jobe");
-      var myobj = { email: req.body.email};
-        dbo.collection("User").insertOne(myobj, function(err, res) {
+      var dbo = await db.db("Ject_Jobe");
+      var myobj = await { email: req.body.email};
+      var Check = await dbo.collection("User").findOne( myobj ) 
+      if(Check){ 
+        console.log(myobj)
+        return res.status(400).json("MEE LAWW")}    
+        
+        await  dbo.collection("User").insertOne(myobj, function(err, res) {
           if (err) throw err;
-          console.log("Created Data");      
+          console.log("Created Data");
+       
       });  
       res.send("Hello World");
     });
