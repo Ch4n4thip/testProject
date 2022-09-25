ae30f1924ce43db38e956134193d57a1d9e1b0ae
TwinCasterS <darktwin224@gmail.com>
2022-09-25 12:51:06 +0700

    SideNav

--- pages/SideNavBar/sideNav.js
+++ pages/SideNavBar/sideNav.js
@@ -1,24 +1,26 @@
-
-import Image from 'next/image'
-import React from "react";
-import {GrContact} from 'react-icons/gr'
+import React from 'react'
+import {GrContact,GrHome,GrCart} from 'react-icons/gr'
+import {RiCoupon2Line , RiScales3Line} from 'react-icons/ri'
 import {SiAboutdotme} from  'react-icons/si'
+import {BiCommentDetail} from 'react-icons/bi'
 import {GiSkills, GiSmallFire} from 'react-icons/gi'
 import {MdOutlineDesignServices,MdHome,MdPermContactCalendar} from 'react-icons/md'
+import {AiOutlineHistory} from 'react-icons/ai'
+import {FiFilter} from 'react-icons/fi'
+import { useState } from 'react'
 import styles1 from './sideNav.module.css'
-
-
-export default function sideNav() {
-    return (
-        <nav className={styles1.nav}>
-              <h1>Test</h1>
-              <Link href="#">  asd <MdHome/>   </Link>
-              <Link href="#">   asd<SiAboutdotme/> </Link>
-              <Link href="#">  asd <MdOutlineDesignServices/>  </Link>
-              <Link href="#">  asd <MdOutlineDesignServices/>  </Link>
-              <Link href="#">  asd <MdPermContactCalendar/>    </Link>
-        </nav>
-      )
+const sideNav = () => {
+  const [activeNav,setActive] = useState('#');
+  return (
+    <nav className={styles1.Nav}>
+      <a href="#"  onClick={()=>setActive('#')} className={activeNav === '#' ? 'active' : ''}><GrHome/></a>
+      <a href="#about"  onClick={()=>setActive('#about')} className={activeNav === '#about' ? 'active' : ''} ><GrCart/></a>   
+      <a href="#exp"  onClick={()=>setActive('#exp')} className={activeNav === '#exp' ? 'active' : ''} ><RiCoupon2Line/></a>    
+      <a href="#services"   onClick={()=>setActive('#services')} className={activeNav === '#services' ? 'active' : ''} ><RiScales3Line/></a>      
+      <a href="#contact"  onClick={()=>setActive('#contact')} className={activeNav === '#contact' ? 'active' : ''} ><BiCommentDetail/></a>
+      <a href="#contact"  onClick={()=>setActive('#contact')} className={activeNav === '#contact' ? 'active' : ''} ><AiOutlineHistory/></a>
+      <a href="#contact"  onClick={()=>setActive('#contact')} className={activeNav === '#contact' ? 'active' : ''} ><FiFilter/></a>
+    </nav>
+  )
 }
-
-
+export default sideNav
